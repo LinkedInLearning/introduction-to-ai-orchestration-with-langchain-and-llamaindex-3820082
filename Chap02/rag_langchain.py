7724b04a215cf2bad920816d2ff86129f45f3da1
Micah Dubinko <962188+mdubinko@users.noreply.github.com>
2023-12-12 14:10:30 -0800

    First draft of Chap02 code

---
+++ Chap02/rag_langchain.py
@@ -0,0 +1,82 @@
+import argparse
+import os
+from langchain.chains import ConversationalRetrievalChain
+# pip install unstructured[rst]
+from langchain.chat_models.openai import ChatOpenAI
+from langchain.document_loaders import DirectoryLoader, Docx2txtLoader
+from langchain.embeddings import SentenceTransformerEmbeddings
+from langchain.memory import ConversationBufferMemory
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+# pip install faiss-gpu # For CUDA 7.5+ Supported GPU
+# OR
+# pip install faiss-cpu # For CPU Installation
+from langchain.vectorstores.faiss import FAISS
+import os
+
+os.environ["TOKENIZERS_PARALLELISM"] = "false" # workaround for HuggingFace/tokenizers
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--docs_dir", type=str, default="./handbook/")
+    parser.add_argument("--persist_dir", type=str, default="handbook_faiss")
+    args = parser.parse_args()
+
+    print(f"Using data dir {args.docs_dir}")
+    print(f"Using index path {args.persist_dir}")
+    
+    embedding = SentenceTransformerEmbeddings(model_name="all-mpnet-base-v2")
+    print(f"Embedding: {embedding.model_name}")
+
+    if os.path.exists(args.persist_dir): 
+        print(f"Loading FAISS index from {args.persist_dir}")
+        vectorstore = FAISS.load_local(args.persist_dir, embedding)
+        print("done.")
+    else:
+        print(f"Building FAISS index from documents in {args.docs_dir}")
+        loader = DirectoryLoader(args.docs_dir,
+            loader_cls=Docx2txtLoader,
+            recursive=True,
+            silent_errors=True,
+            show_progress=True,
+            glob="**/*.docx"  # which files get loaded
+        )
+        docs = loader.load()
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=75)
+        frags = text_splitter.split_documents(docs)
+
+        print(f"Poplulating vector store with {len(docs)} docs in {len(frags)} fragments")
+        vectorstore = FAISS.from_documents(frags, embedding)
+        print(f"Persisting vector store to: {args.persist_dir}")
+        vectorstore.save_local(args.persist_dir)
+        print(f"Saved FAISS index to {args.persist_dir}")
+
+    # Be sure your local model suports a large context size for this
+    llm = ChatOpenAI(
+        base_url="http://localhost:1234/v1",
+        temperature=0.6
+    )
+
+    memory = ConversationBufferMemory(
+        memory_key="chat_history",
+        return_messages=True
+    )
+    print(memory.load_memory_variables({}))
+    qa_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        memory=memory,
+        retriever=vectorstore.as_retriever()
+    )
+
+    # Start a REPL loop
+    while True:
+        user_input = input("Ask a question. Type 'exit' to quit.\n>")
+        if user_input=="exit":
+            break
+        memory.chat_memory.add_user_message(user_input)
+        result = qa_chain({"question": user_input})
+        response = result["answer"]
+        memory.chat_memory.add_ai_message(response)
+        print("AI:", response)
+
+if __name__ == "__main__":
+    main()
---
+++ Chap02/rag_llama.py
@@ -0,0 +1,93 @@
+import argparse
+from llama_index import (
+    load_index_from_storage, set_global_service_context
+)
+from llama_index import ServiceContext
+from llama_index.chat_engine import ContextChatEngine
+from llama_index.embeddings import HuggingFaceEmbedding
+from llama_index.indices.vector_store import VectorStoreIndex
+from llama_index.llms import OpenAILike
+from llama_index.query_engine import RetrieverQueryEngine
+# pip install docx2txt to read Microsoft Word files
+from llama_index.readers import SimpleDirectoryReader
+from llama_index.retrievers import VectorIndexRetriever
+from llama_index.storage import StorageContext
+import os
+
+os.environ["TOKENIZERS_PARALLELISM"] = "false" # workaround for HuggingFace FastTokenizers
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--docs_dir", type=str, default="./handbook/", help="Directory containing documents to index")
+    parser.add_argument("--persist_dir", type=str, default="./handbook_index/", help="Path to store the serialized VectorStore")
+    args = parser.parse_args()
+
+    print(f"Using data dir {args.docs_dir}")
+    print(f"Using index path {args.persist_dir}")
+
+    embed_model=HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
+    print(f"Embedding: {embed_model.model_name}")
+
+    llm=OpenAILike(
+        model="local",
+        is_chat_model=True,
+        api_base="http://localhost:1234/v1",  # see chapter 1 to configure local LLM
+        temperature=0.6,
+    )
+
+    service_context = ServiceContext.from_defaults(
+        llm=llm,
+        chunk_size=512,
+        chunk_overlap=64,
+        embed_model=embed_model
+    )
+    set_global_service_context(service_context)
+    # TODO: comment out unneeded explicit references to service_context
+    # (but leave in for documentation purposes)
+
+    # Load or create the VectorStore
+    vector_store = None
+    if os.path.exists(args.persist_dir):
+        print(f"Reading VectorStore from {args.persist_dir}")
+        storage_context = StorageContext.from_defaults(
+            persist_dir=args.persist_dir,
+        )
+        vector_store = load_index_from_storage(
+            storage_context=storage_context,
+            service_context=service_context
+            )
+        print("done")
+    else:
+        print(f"Reading documents in: {args.docs_dir}")
+        documents = SimpleDirectoryReader(args.docs_dir).load_data()
+
+        # production apps may require a more tailored approach to loading & splitting docs
+
+        vector_store = VectorStoreIndex.from_documents(documents, service_context=service_context)
+        print(f"Persisting vector store to: {args.persist_dir}")
+        os.mkdir(args.persist_dir)
+        vector_store.storage_context.persist(persist_dir=args.persist_dir)
+        vector_store
+        print("done")
+
+    print(f"setting up service context using {embed_model.model_name}")
+
+    retriever = VectorIndexRetriever(vector_store)
+    query_engine = RetrieverQueryEngine.from_args(
+        retriever=retriever,
+        service_context=service_context
+    )
+
+    chat_engine = ContextChatEngine.from_defaults(
+        retriever=retriever,
+        query_engine=query_engine,
+        service_context=service_context,
+        verbose=True
+    )
+
+    # Main chat loop: try "What holidays are PTO?"
+    chat_engine.chat_repl()
+
+
+if __name__ == "__main__":
+    main()
