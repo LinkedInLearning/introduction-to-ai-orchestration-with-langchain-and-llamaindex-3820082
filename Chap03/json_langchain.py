a2e852efff460710516eebe2d0604814de62aee6
Micah Dubinko <962188+mdubinko@users.noreply.github.com>
2023-12-13 10:40:48 -0800

    3.2 code examples

---
+++ Chap03/json_langchain.py
@@ -0,0 +1,66 @@
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema import HumanMessage, SystemMessage
+from langchain.schema.runnable.config import RunnableConfig
+
+prompt = """Write a weather report for a major city
+    in ten words or less.
+    Do not include any additional explanation.
+"""
+
+guided_prompt = prompt + """
+Return the result as JSON as follows:
+{ "city": "<CITY_NAME>",
+"report": "<BRIEF_REPORT>" }
+"""
+
+chat = ChatOpenAI(
+    #base_url="http://localhost:1234/v1",
+    temperature=0.7,
+    max_tokens=500,
+    model='gpt-4-1106-preview'
+)
+
+def baseline():
+    print("baseline:")
+    print(chat.invoke(prompt).content)
+
+def with_guided_prompt():
+    print("1. Ask nicely")
+    print(chat.invoke(guided_prompt).content)
+
+def with_pydantic_output_formatter():
+    print("2. Pydantic OutputParser")
+    from langchain.output_parsers import PydanticOutputParser
+    from langchain.pydantic_v1 import BaseModel, Field
+
+    class WeatherReport(BaseModel):
+        city: str = Field(description="City name")
+        report: str = Field(description="Brief weather report")
+
+    parser = PydanticOutputParser(pydantic_object=WeatherReport)
+    #print(f"Parser instructions: {parser.get_format_instructions()}")
+
+    runnable_prompt = ChatPromptTemplate.from_messages(
+        [
+            SystemMessage(content=parser.get_format_instructions()),
+            HumanMessage(content=prompt)
+        ]
+    )
+    chain = runnable_prompt | chat | parser
+    py_obj = chain.invoke({})
+    print(py_obj.city, py_obj.report)
+
+    #To be extra robust about fixing JSON errors you can add
+    #from langchain.output_parsers import OutputFixingParser
+    #robust_parser = OutputFixingParser.from_llm(
+    #    parser=parser,
+    #    llm=chat
+    #)
+    # this will re-prompt to get a conforming output format
+
+
+if __name__ == "__main__":
+    #baseline()
+    #with_guided_prompt()
+    with_pydantic_output_formatter()
---
+++ Chap03/json_llamaindex.py
@@ -0,0 +1,59 @@
+from llama_index.llms import ChatMessage, OpenAILike
+
+prompt = ChatMessage(
+    role="user",
+    content="""Write a weather report for a random city
+        in ten words or less.
+        Do not include any additional explanation.
+""")
+
+guided_prompt = ChatMessage(role="user", content=prompt.content + """
+Return the result as JSON as follows:
+{ "city": "<CITY_NAME>",
+"report": "<SHORT_REPORT>" }
+""")
+
+chat = OpenAILike(
+    is_chat_model=True,
+    is_function_calling_model=True,
+    #api_base="http://localhost:1234/v1",
+    temperature=0.7,
+    max_tokens=500,
+    model="gpt-4-1106-preview",
+)
+
+def baseline():
+    print("baseline:")
+    print(chat.chat([prompt]))
+
+def with_guided_prompt():
+    print("1. Ask nicely")
+    print(chat.chat([guided_prompt]))
+
+def with_openai_pydantic():
+    print("2. OpenAIPydandicProgram")
+    from pydantic import BaseModel, Field
+    from llama_index.program import OpenAIPydanticProgram
+
+    class WeatherReport(BaseModel):
+        "A concise weather report for a single city"
+        city: str = Field(description="City name")
+        report: str = Field(description="Brief weather report")
+
+    program = OpenAIPydanticProgram.from_defaults(
+        llm=chat,
+        output_cls=WeatherReport,
+        prompt_template_str=prompt.content,
+        verbose=True,
+    )
+    print(guided_prompt.content)
+    py_obj = program()
+    # Now a standard python obj
+    print(py_obj.city, py_obj.report)
+
+
+if __name__ == "__main__":
+    baseline()
+    with_guided_prompt()
+    with_openai_pydantic()
+
