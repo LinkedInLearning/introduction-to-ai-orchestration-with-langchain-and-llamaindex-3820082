1b4bcfa261be68c4e178ef99a32f87c62fb317d5
Micah Dubinko <962188+mdubinko@users.noreply.github.com>
2023-12-14 10:05:14 -0800

    Chap04 example files

---
+++ Chap04/04_03_challenge.py
@@ -0,0 +1,44 @@
+from langchain.agents import AgentType, Tool, initialize_agent
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+
+#  In order to set this Bing search, go to
+# https://www.microsoft.com/en-us/bing/apis/bing-web-search-api
+# and click "Try now" to get a Bing web search API key
+# put your API key in the env variable BING_SUBSCRIPTION_KEY
+from langchain.utilities.bing_search import BingSearchAPIWrapper
+
+import os
+os.environ["BING_SEARCH_URL"] = "https://api.bing.microsoft.com/v7.0/search"
+
+llm = ChatOpenAI(
+    base_url="http://localhost:1234/v1/",
+    temperature=0
+)
+
+tools = [   
+    Tool(
+        name="Web Search",
+        func=BingSearchAPIWrapper().run,
+        description="useful for when you need to answer specific questions from information on the web",
+    )
+]
+
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True, output_key="output")
+
+agent = initialize_agent(
+    tools,
+    llm,
+    agent=AgentType.CHAT_CONVERSATIONAL_REACT_DESCRIPTION,
+    verbose=True,
+    memory=memory,
+    handle_parsing_errors=True
+)
+
+
+pydict = agent.invoke({"input": "Hi, I am Bob"})
+print(pydict["output"])
+pydict = agent.invoke({"input": "What's my name?"})
+print(pydict["output"])
+pydict = agent.invoke({"input": "Who is the CEO of LinkedIn in 2023?"})
+print(pydict["output"])
---
+++ Chap04/agent_llamaindex.py
@@ -0,0 +1,32 @@
+from llama_index.tools import QueryEngineTool, ToolMetadata
+from llama_index.agent import ReActAgent
+from llama_index.llms import OpenAILike
+# pip install llama_hub
+# In order to set this Bing search, go to
+# https://www.microsoft.com/en-us/bing/apis/bing-web-search-api
+# and click "Try now" to get a Bing web search API key
+# put your API key in the env variable BING_SUBSCRIPTION_KEY
+from llama_hub.tools.bing_search import BingSearchToolSpec
+
+import os
+api_key = os.environ["BING_SUBSCRIPTION_KEY"]
+
+tool_spec = BingSearchToolSpec(api_key=api_key)
+
+tool_list = tool_spec.to_tool_list()
+
+llm = OpenAILike(
+    is_chat_model=True,
+    model="gpt-4-1106-preview",
+    api_base="http://localhost:1234/v1/"
+)
+agent = ReActAgent.from_tools(tool_list, llm=llm, verbose=True)
+
+response = agent.chat("Hi, I am Bob.")
+print(str(response))
+
+response = agent.chat("What's my name?")
+print(str(response))
+
+response = agent.chat("Who is the CEO of LinkedIn in 2023?")
+print(str(response))
